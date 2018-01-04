fb05feb038137a7215a7f7c14d4b06dbd9da312d
Wyko Rijnsburger <wrijnsburger@bol.com>
2018-01-04 08:59:28 +0100

    Add support for lists, booleans, floats, double and longs.

--- src/main/kotlin/nl/wykorijnsburger/minimaltestobjects/MinRandom.kt
+++ src/main/kotlin/nl/wykorijnsburger/minimaltestobjects/MinRandom.kt
@@ -4,25 +4,37 @@ import java.util.*
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createType
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.full.starProjectedType
+import kotlin.reflect.jvm.jvmErasure
 import kotlin.streams.asSequence
 
 private val random = Random()
 
-fun <T : Any> minRandom(type: KClass<T>): T? {
-    val parameters = type.primaryConstructor!!.parameters
+fun <T : Any> KClass<T>.minRandom() = generateMinRandom(this)
+
+fun <T : Any> generateMinRandom(clazz: KClass<T>): T {
+    val parameters = clazz.primaryConstructor!!.parameters
 
     val constructorArguments = parameters
             .map {
                 val type = it.type
+                val listType = it.type.classifier?.starProjectedType
                 when {
                     type.isMarkedNullable -> null
                     type == String::class.createType() -> randomString()
                     type == Int::class.createType() -> random.nextInt()
-                    else -> null
+                    type == Long::class.createType() -> random.nextLong()
+                    type == Float::class.createType() -> random.nextFloat()
+                    type == Double::class.createType() -> random.nextDouble()
+                    type == Boolean::class.createType() -> random.nextBoolean()
+                    listType == List::class.starProjectedType -> {
+                        emptyList<Any>()
+                    }
+                    else -> generateMinRandom(type.jvmErasure)
                 }
             }
 
-    return type.primaryConstructor?.call(*constructorArguments.toTypedArray())
+    return clazz.primaryConstructor!!.call(*constructorArguments.toTypedArray())
 }
 
 private fun randomString(): String {
--- src/test/kotlin/nl/wykorijnsburger/minimaltestobjects/MinRandomTest.kt
+++ src/test/kotlin/nl/wykorijnsburger/minimaltestobjects/MinRandomTest.kt
@@ -1,18 +1,49 @@
 package nl.wykorijnsburger.minimaltestobjects
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-
 
 class MinRandomTest {
 
     @Test
-    fun `Test`() {
-        val randomPerson = minRandom(Person::class)
+    fun `Should generate nullable fields as null`() {
+        val randomDC = generateMinRandom(DC::class)
 
-        println("randomPerson = $randomPerson")
+        assertThat(randomDC.nullableString).isNull()
+        assertThat(randomDC.nullableInt).isNull()
+        assertThat(randomDC.nullableListOfStrings).isNull()
     }
 
-    data class Person(val id: String,
-                      val age: Int,
-                      val firstName: String?)
+    @Test
+    fun `Should generate min random objects using extension method`() {
+        val randomDC = DC::class.minRandom()
+
+        assertThat(randomDC).isNotNull()
+    }
+
+    @Test
+    fun `Should generate lists as empty lists`() {
+        val randomDC = DC::class.minRandom()
+
+        assertThat(randomDC.listOfStrings).isEmpty()
+    }
+
+    @Test
+    fun `Should generate nested DCs`() {
+        val randomDCWithNestedDC = DCWithNestedDC::class.minRandom()
+
+        assertThat(randomDCWithNestedDC.nestedDC.normalField).isNotNull()
+    }
+
+    data class DC(val nonNullableString: String,
+                  val nullableString: String?,
+                  val nonNullableInt: Int,
+                  val nullableInt: Int?,
+                  val listOfStrings: List<String>,
+                  val nullableListOfStrings: List<String>?)
+
+    data class DCWithNestedDC(val normalField: String,
+                              val nestedDC: NestedDC)
+
+    data class NestedDC(val normalField: String)
 }
