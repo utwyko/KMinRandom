dab98321bedd44c099b630873def579891efc0ea
Wyko Rijnsburger <utwyko@gmail.com>
2018-12-02 20:46:44 +0100

    Use the maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     `java-library`
+    `maven-publish`
+    signing
     kotlin("jvm") version Versions.kotlin
     id("com.github.ben-manes.versions") version Versions.benManesVersions
+}
+
+group = "nl.wykorijnsburger.kminrandom"
+version = "0.0.8"
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
@@ -23,6 +32,60 @@ tasks.withType<Wrapper> {
     distributionType = Wrapper.DistributionType.ALL
 }
 
-repositories {
-    mavenCentral()
+task<Jar>("sourcesJar") {
+    from(sourceSets.main.get().allJava)
+    classifier = "sources"
+}
+
+task<Jar>("javadocJar") {
+    from(tasks.javadoc)
+    classifier = "javadoc"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "kminrandom"
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set("KMinRandom")
+                description.set("A library for generating minimal random instances of Kotlin data classes")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("wyko")
+                        name.set("Wyko Rijnsburger")
+                    }
+                }
+                url.set("https://github.com/utwyko/KMinRandom")
+                scm {
+                    url.set("https://github.com/utwyko/KMinRandom")
+                    connection.set("scm:git:git://github.com/utwyko/KMinRandom.git")
+                    developerConnection.set("scm:git:ssh://example.com/my-library.git")
+                }
+            }
+        }
+        repositories {
+            maven {
+                name = "sonatype"
+                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = property("NEXUS_USERNAME") as String
+                    password = property("NEXUS_PASSWORD") as String
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
