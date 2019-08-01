2e65a8373b4f0d5fd0c8da5ceb169ee67df847e9
Wyko Rijnsburger <utwyko@gmail.com>
2019-08-01 20:46:31 +0200

    KT Lint Gradle 8.1.0 -> 8.2.0.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val assertJ = "3.13.1"
     const val kotlin = "1.3.41"
     const val benManesVersions = "0.21.0"
-    const val ktlintGradlePluginVersion = "8.1.0"
+    const val ktlintGradlePluginVersion = "8.2.0"
 }
 
 object Plugins {
