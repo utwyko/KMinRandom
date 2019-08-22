b1f2c362e1b204bb884cc602cc406242fa391bfa
Wyko Rijnsburger <utwyko@gmail.com>
2019-08-22 19:57:09 +0200

    assertJ 3.13.1 -> 3.13.2.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val junitJupiter = "5.5.1"
-    const val assertJ = "3.13.1"
+    const val assertJ = "3.13.2"
     const val kotlin = "1.3.41"
     const val benManesVersions = "0.22.0"
     const val ktlintGradlePluginVersion = "8.2.0"
