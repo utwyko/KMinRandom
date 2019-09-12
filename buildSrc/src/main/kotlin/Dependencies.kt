358f62395d5a844597bf1fff8cdcfaf23ddb2f2b
Wyko Rijnsburger <utwyko@gmail.com>
2019-09-12 19:34:22 +0200

    Versions Plugin 0.22.0 -> 0.25.0.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ object Versions {
     const val junitJupiter = "5.5.1"
     const val assertJ = "3.13.2"
     const val kotlin = "1.3.50"
-    const val benManesVersions = "0.22.0"
+    const val benManesVersions = "0.25.0"
     const val ktlintGradlePluginVersion = "8.2.0"
 }
 
