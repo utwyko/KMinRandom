d46bad67da1cbbe1d7957cf714ba634422e68bc1
Wyko Rijnsburger <utwyko@gmail.com>
2020-03-31 10:44:14 +0200

    JUnit Jupiter 5.6.0 -> 5.6.1.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val junitJupiter = "5.6.0"
+    const val junitJupiter = "5.6.1"
     const val assertJ = "3.15.0"
     const val kotlin = "1.3.70"
     const val benManesVersions = "0.28.0"
