398e0ac27e7b300e05aec38b69480f9981eddf9b
Wyko Rijnsburger <utwyko@gmail.com>
2020-05-21 09:30:05 +0200

    Kotlin 1.3.71 -> 1.3.72.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val junitJupiter = "5.6.1"
     const val assertJ = "3.15.0"
-    const val kotlin = "1.3.71"
+    const val kotlin = "1.3.72"
     const val benManesVersions = "0.28.0"
     const val ktlintGradlePluginVersion = "9.2.1"
 }
