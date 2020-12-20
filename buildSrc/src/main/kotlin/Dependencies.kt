2826cbab60cbb013f212b611491d4b99639605a0
Wyko Rijnsburger <utwyko@gmail.com>
2020-12-20 13:15:06 +0100

    Gradle Versions 0.33.0 -> 0.36.0.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ object Versions {
     const val junitJupiter = "5.7.0"
     const val assertJ = "3.17.2"
     const val kotlin = "1.4.21"
-    const val benManesVersions = "0.33.0"
+    const val benManesVersions = "0.36.0"
     const val ktlintGradlePluginVersion = "9.4.1"
 }
 
