f23da22aafc1fb5e1e0c0c1a0a20874ee26a1858
Wyko Rijnsburger <utwyko@gmail.com>
2021-05-17 10:07:41 +0200

    Kotlin 1.4.31 -> 1.5.0.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ enableFeaturePreview("VERSION_CATALOGS")
 // Plugin versions cannot be specified in the versions catalog yet, see https://melix.github.io/blog/2021/03/version-catalogs-faq.html
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.4.31"
+        kotlin("jvm") version "1.5.0"
         id("com.github.ben-manes.versions") version "0.38.0"
         id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
     }
