9605e13d189364e35503c2a04c1c2c4a0b6e853c
Wyko Rijnsburger <utwyko@gmail.com>
2026-01-18 21:48:43 +0100

    Migrate to gradle-maven-publish-plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    `maven-publish`
-    signing
     alias(libs.plugins.kotlin.gradlePluginJvm)
     alias(libs.plugins.detekt)
     alias(libs.plugins.gradleVersionsPlugin)
+    alias(libs.plugins.mavenPublish)
 }
 
 group = "nl.wykorijnsburger.kminrandom"
@@ -52,62 +51,39 @@ tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
 
-tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.main.get().allJava)
-    archiveClassifier.set("sources")
-}
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
 
-tasks.register<Jar>("javadocJar") {
-    from(tasks.javadoc)
-    archiveClassifier.set("javadoc")
-}
+    // Only sign when credentials are available (CI/release builds)
+    if (project.hasProperty("signing.keyId") || System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKey") != null) {
+        signAllPublications()
+    }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            artifactId = "kminrandom"
-            from(components["java"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
-            pom {
-                name.set("KMinRandom")
-                description.set("A library for generating minimal random instances of Kotlin data classes")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("wyko")
-                        name.set("Wyko Rijnsburger")
-                    }
-                }
-                url.set("https://github.com/utwyko/KMinRandom")
-                scm {
-                    url.set("https://github.com/utwyko/KMinRandom")
-                    connection.set("scm:git:git://github.com/utwyko/KMinRandom.git")
-                    developerConnection.set("scm:git:ssh://example.com/my-library.git")
-                }
+    coordinates("nl.wykorijnsburger.kminrandom", "kminrandom", version.toString())
+
+    pom {
+        name.set("KMinRandom")
+        description.set("A library for generating minimal random instances of Kotlin data classes")
+        url.set("https://github.com/utwyko/KMinRandom")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
             }
         }
-        repositories {
-            maven {
-                name = "sonatype"
-                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = findProperty("NEXUS_USERNAME") as String?
-                    password = findProperty("NEXUS_PASSWORD") as String?
-                }
+        developers {
+            developer {
+                id.set("wyko")
+                name.set("Wyko Rijnsburger")
             }
+        }
+        scm {
+            url.set("https://github.com/utwyko/KMinRandom")
+            connection.set("scm:git:git://github.com/utwyko/KMinRandom.git")
+            developerConnection.set("scm:git:ssh://github.com/utwyko/KMinRandom.git")
         }
     }
-}
-
-signing {
-    sign(publishing.publications["mavenJava"])
 }
 
 detekt {
